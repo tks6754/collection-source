d123b19d806dbf80dc3cc486c4e697a9c93740b9
MiaoKun <tks6754@gmail.com>
2016-10-10 17:46:59 +0800

    update

---
+++ src/main/java/com/collections/list/ListDemo.java
@@ -0,0 +1,13 @@
+package com.collections.list;
+
+import java.util.List;
+
+/**
+ * Created by miaokun on 2016/10/10.
+ */
+public class ListDemo {
+    public void main(String[] args){
+        List list;
+    }
+
+}
